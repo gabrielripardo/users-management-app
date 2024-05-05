c15671dae8b4b275cd6d9ed7f6a00c3cc7962347
gts.senna <gts.senna@gmail.com>
2024-05-04 23:48:45 -0300

    FEAT: estrutura da lista e rota para formulário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'user-form',
+    loadChildren: () => import('./pages/user-form/user-form.module').then(m => m.UserFormPageModule)
   }
 ];
 @NgModule({
